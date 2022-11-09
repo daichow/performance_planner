ae52286304d8ce5c1dfc50ead4bbf048c75683a6
daichow <daiansask@gmail.com>
2022-11-09 15:09:21 -0500

    now trying to get the cartesian path planner to work

--- gen3_configuration/scripts/mission_action_server.py
+++ gen3_configuration/scripts/mission_action_server.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# from __future__ import print_function
-# from six.moves import input
-
 import sys
 import copy
 import rospy
@@ -51,81 +48,66 @@ class MissionActionServer(object):
 
     def __init__(self):
         super(MissionActionServer, self).__init__()
-
-        ## BEGIN_SUB_TUTORIAL setup
-        ##
-        ## First initialize `moveit_commander`_ and a `rospy`_ node:
-        # self.joint_state_topic = ['joint_states:=/my_gen3/joint_states']
         moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node("mission_as", anonymous=True)
+        rospy.init_node("mission_as", )
 
-        self.degrees_of_freedom = rospy.get_param(
-               "my_gen3" + "/degrees_of_freedom", 6)
+        try:
+            self.degrees_of_freedom = rospy.get_param(rospy.get_namespace() + "degrees_of_freedom", 6)
 
-        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-        ## kinematic model and the robot's current joint states
-        robot = moveit_commander.RobotCommander("my_gen3" + "/robot_description")
+            arm_group_name = "arm"
+            self.robot = moveit_commander.RobotCommander("my_gen3/robot_description")
+            self.scene = moveit_commander.PlanningSceneInterface(ns=rospy.get_namespace())
+            self.move_group = moveit_commander.MoveGroupCommander(arm_group_name, ns=rospy.get_namespace())
+            self.display_trajectory_publisher = rospy.Publisher(rospy.get_namespace() + 'move_group/display_planned_path',
+                                                            moveit_msgs.msg.DisplayTrajectory,
+                                                            queue_size=20)
 
-        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-        ## to a planning group (group of joints).  In this tutorial the group is the primary
-        ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
-        ## If you are using a different robot, change this value to the name of your robot
-        ## arm planning group.
-        ## This interface can be used to plan and execute motions:
-        group_name = "arm"
-        # print(f"apple pie {"my_gen3"}")
-        move_group = moveit_commander.MoveGroupCommander(robot_description="my_gen3/robot_description", ns="my_gen3",
-                                                                 name=group_name)
+            rospy.loginfo("Initializing node in namespace " + rospy.get_namespace())
+        except Exception as e:
+            print (e)
+        else:
+            print("successfully initialized")
 
-        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
-        ## for getting, setting, and updating the robot's internal understanding of the
-        ## surrounding world:
-        scene = moveit_commander.PlanningSceneInterface(
-                ns="my_gen3")
+    def get_cartesian_pose(self):
+        arm_group = self.move_group
 
-        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
-        ## trajectories in Rviz:
-        display_trajectory_publisher = rospy.Publisher(
-            "my_gen3" + "/move_group/display_planned_path",
-            moveit_msgs.msg.DisplayTrajectory,
-            queue_size=20,
-        )
+        # Get the current pose and display it
+        pose = arm_group.get_current_pose()
+        rospy.loginfo("Actual cartesian pose is : ")
+        rospy.loginfo(pose.pose)
 
-        ## END_SUB_TUTORIAL
+        return pose.pose
+    
+    def reach_joint_angles(self, tolerance, joint_positions=[]):
+        arm_group = self.move_group
+        success = True
 
-        ## BEGIN_SUB_TUTORIAL basic_info
-        ##
-        ## Getting Basic Information
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-        # We can get the name of the reference frame for this robot:
-        planning_frame = move_group.get_planning_frame()
-        print("============ Planning frame: %s" % planning_frame)
+        # Get the current joint positions
+        # joint_positions = arm_group.get_current_joint_values()
+        # rospy.loginfo("Printing current joint positions before movement :")
+        # for p in joint_positions: rospy.loginfo(p)
 
-        # We can also print the name of the end-effector link for this group:
-        eef_link = move_group.get_end_effector_link()
-        print("============ End effector link: %s" % eef_link)
+        # Set the goal joint tolerance
+        arm_group.set_goal_joint_tolerance(tolerance)
 
-        # We can get a list of all the groups in the robot:
-        group_names = robot.get_group_names()
-        print("============ Available Planning Groups:", robot.get_group_names())
+        # Set the joint target configuration
+        # joint_positions[0] = 0
+        # joint_positions[1] = 0
+        # joint_positions[2] = pi/2
+        # joint_positions[3] = pi/4
+        # joint_positions[4] = 0
+        # joint_positions[5] = pi/2
+        arm_group.set_joint_value_target(joint_positions)
+        
+        # Plan and execute in one command
+        success &= arm_group.go(wait=True)
 
-        # Sometimes for debugging it is useful to print the entire state of the
-        # robot:
-        print("============ Printing robot state")
-        print(robot.get_current_state())
-        print("")
-        ## END_SUB_TUTORIAL
-
-        # Misc variables
-        self.box_name = ""
-        self.robot = robot
-        self.scene = scene
-        self.move_group = move_group
-        self.display_trajectory_publisher = display_trajectory_publisher
-        self.planning_frame = planning_frame
-        self.eef_link = eef_link
-        self.group_names = group_names
-
+        # Show joint positions after movement
+        # new_joint_positions = arm_group.get_current_joint_values()
+        # rospy.loginfo("Printing current joint positions after movement :")
+        # for p in new_joint_positions: rospy.loginfo(p)
+        return success
+    
     def reach_named_position(self, target):
         """ This function makes the robot reach a named position defined in the 
         moveit wizard"""
@@ -135,6 +117,7 @@ class MissionActionServer(object):
         # Going to one of those targets
         rospy.loginfo("Going to named target " + target)
         # Set the target
+        print(f"All possible named target values: {arm_group.get_named_targets()}")
         arm_group.set_named_target(target)
         arm_group.set_max_velocity_scaling_factor(1.0)
         arm_group.set_max_acceleration_scaling_factor(1.0)
@@ -145,60 +128,70 @@ class MissionActionServer(object):
         return arm_group.execute(trajectory_message, wait=True)
 
     def go_to_pose_goal(self):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
         move_group = self.move_group
 
-        ## BEGIN_SUB_TUTORIAL plan_to_pose
-        ##
-        ## Planning to a Pose Goal
-        ## ^^^^^^^^^^^^^^^^^^^^^^^
-        ## We can plan a motion for this group to a desired pose for the
-        ## end-effector:
         pose_goal = geometry_msgs.msg.Pose()
         pose_goal.orientation.w = 1.0
         pose_goal.position.x = 0.4
         pose_goal.position.y = 0.1
         pose_goal.position.z = 0.4
 
+        # pose_goal = geometry_msgs.msg.PoseStamped()
+        # pose_goal.header.frame_id = "world"
+        # pose_goal.pose.position.x = 0.2596238372665062
+        # pose_goal.pose.position.y = 0.5877850010083194
+        # pose_goal.pose.position.z = 1.0238443039530603
+        # pose_goal.pose.orientation.x = -0.5055357895603413
+        # pose_goal.pose.orientation.y = 0.4986347640062258
+        # pose_goal.pose.orientation.z = 0.5102174540797526
+        # pose_goal.pose.orientation.w = 0.48525775331305615
+
         move_group.set_pose_target(pose_goal)
 
-        ## Now, we call the planner to compute the plan and execute it.
-        # `go()` returns a boolean indicating whether the planning and execution was successful.
         success = move_group.go(wait=True)
-        # Calling `stop()` ensures that there is no residual movement
+
         move_group.stop()
-        # It is always good to clear your targets after planning with poses.
-        # Note: there is no equivalent function for clear_joint_value_targets().
+
         move_group.clear_pose_targets()
 
-        ## END_SUB_TUTORIAL
-
-        # For testing:
-        # Note that since this section of code will not be included in the tutorials
-        # we use the class variable rather than the copied state variable
         current_pose = self.move_group.get_current_pose().pose
         return all_close(pose_goal, current_pose, 0.01)
+    
+    def reach_cartesian_pose(self, pose, tolerance, constraints):
+        arm_group = self.move_group
+        
+        # Set the tolerance
+        arm_group.set_goal_position_tolerance(tolerance)
+
+        # Set the trajectory constraint if one is specified
+        if constraints is not None:
+            arm_group.set_path_constraints(constraints)
+
+        # Get the current Cartesian Position
+        arm_group.set_pose_target(pose)
+
+        # Plan and execute
+        rospy.loginfo("Planning and going to the Cartesian Pose")
+        return arm_group.go(wait=True)
 
     def plan_cartesian_path(self, scale=1):
 
         move_group = self.move_group
 
-        move_group.set_max_velocity_scaling_factor(0.03)
-        move_group.set_max_acceleration_scaling_factor(0.01)
+        # move_group.set_max_velocity_scaling_factor(0.03)
+        # move_group.set_max_acceleration_scaling_factor(0.01)
         move_group.set_num_planning_attempts(10)
 
         waypoints = []
 
         mtarget = moveit_commander.Pose()
-        mtarget.position.x = 0.2596238372665062
-        mtarget.position.y = 0.5877850010083194
-        mtarget.position.z = 1.0238443039530603
-        mtarget.orientation.x = -0.5055357895603413
-        mtarget.orientation.y = 0.4986347640062258
-        mtarget.orientation.z = 0.5102174540797526
-        mtarget.orientation.w = 0.48525775331305615
+        mtarget.position.x = 0.43822136
+        mtarget.position.y = 0.089
+        mtarget.position.z = 0.8036750679792021
+        mtarget.orientation.x = 0.0006745600176505448
+        mtarget.orientation.y = 0.7134677370703144
+        mtarget.orientation.z = -0.0058675983477976265
+        mtarget.orientation.w = 0.7006631889989489
 
         waypoints.append(mtarget)
 
@@ -212,30 +205,13 @@ class MissionActionServer(object):
         return plan, fraction
 
     def display_trajectory(self, plan):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
         robot = self.robot
         display_trajectory_publisher = self.display_trajectory_publisher
 
-        ## BEGIN_SUB_TUTORIAL display_trajectory
-        ##
-        ## Displaying a Trajectory
-        ## ^^^^^^^^^^^^^^^^^^^^^^^
-        ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
-        ## group.plan() method does this automatically so this is not that useful
-        ## here (it just displays the same trajectory again):
-        ##
-        ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
-        ## We populate the trajectory_start with our current robot state to copy over
-        ## any AttachedCollisionObjects and add our plan to the trajectory.
         display_trajectory = moveit_msgs.msg.DisplayTrajectory()
         display_trajectory.trajectory_start = robot.get_current_state()
         display_trajectory.trajectory.append(plan)
-        # Publish
         display_trajectory_publisher.publish(display_trajectory)
-
-        ## END_SUB_TUTORIAL
 
     def execute_plan(self, plan):
         move_group = self.move_group
@@ -250,33 +226,31 @@ def main():
 
         tutorial = MissionActionServer()
         tutorial.move_group.set_goal_tolerance(0.005)  # 1/2 cm
-        tutorial.move_group.set_planner_id("PRMkConfigDefault")
+        tutorial.move_group.set_planner_id("RRTConnectkConfigDefault")
+        tutorial.move_group.set_planning_time(10)
+        # print(f"current planner id: {tutorial.move_group.get_planner_id()}")
+        # tutorial.move_group.set_planning_pipeline_id("pilz_industrial_motion_planner")
         # tutorial.move_group.set_max_velocity_scaling_factor(0.3)
         # tutorial.move_group.set_max_acceleration_scaling_factor(0.1)
         # tutorial.move_group.set_planning_time(1)
 
-        # tutorial.reach_named_position("home")
-
-        # input(
-        #     "============ Press `Enter` to execute a movement using a joint state goal ..."
-        # )
-        # tutorial.go_to_joint_state()
-
-        # input("============ Press `Enter` to execute a movement using a pose goal ...")
+        # tutorial.reach_named_position("vertical")
+        # tutorial.reach_joint_angles()
         # tutorial.go_to_pose_goal()
-
-        # input("============ Press `Enter` to plan and display a Cartesian path ...")
         cartesian_plan, fraction = tutorial.plan_cartesian_path()
-
-        # input(
-        #     "============ Press `Enter` to display a saved trajectory (this will replay the Cartesian path)  ..."
-        # )
         tutorial.display_trajectory(cartesian_plan)
 
-        # input("============ Press `Enter` to execute a saved path ...")
+        # for point in cartesian_plan.joint_trajectory.points:
+        #     tutorial.reach_joint_angles(0.005, point.positions)
+        # print(cartesian_plan.joint_trajectory.points)
         tutorial.execute_plan(cartesian_plan)
 
-        # print("============ Python tutorial demo complete!")
+
+        # actual_pose = tutorial.get_cartesian_pose()
+        # actual_pose.position.z -= 0.1
+        # # actual_pose.orientation.y += 0.2
+        # tutorial.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+
     except rospy.ROSInterruptException:
         return
     except KeyboardInterrupt:
